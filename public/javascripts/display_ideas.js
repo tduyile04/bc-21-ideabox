402c6a87bc2bca24f1cbfacd27f37a4a2f329bec
Duyile Tolulope Patrick <princericky2000@yahoo.com>
2017-04-19 21:07:52 +0100

    added comments, upvote and downvote icons to the timeline

--- public/javascripts/display_ideas.js
+++ public/javascripts/display_ideas.js
@@ -10,22 +10,27 @@ $(document).ready(function() {
 		ideas = snap.val();
 
 		var keys = Object.keys(ideas);
-		
+
 		console.log(keys);
 
 		for (var i = 0; i < keys.length; i++) {
 			var k = keys[i];
 
 		    var $row = $('<div class="row">' +
-				'<div class="col s3 width1">' +
-					'<td><div>' + ideas[k].Upvotes + '</div>' + 'upvotes' + '</td>' +
-					'<td><div>' + ideas[k].Downvotes + '</div>' +  'downvotes' + '</td>' + 
-					'<td><div>' + ideas[k].Comments + '</div>' +  'comments' + '</td>' + 
+				'<div class="col s1">' +
+					'<td><div>' + ideas[k].Upvotes + '</div>' + '<i class="material-icons">thumb_up</i>' + '</td>' +
 				'</div>' +
-				'<div class="col s6 width2">' +
+				'<div class="col s1">' +
+					'<td><div>' + ideas[k].Downvotes + '</div>' +  '<i class="material-icons">thumb_down</i>' + '</td>' + 
+				'</div>' +
+				'<div class="col s1">' +
+					'<td><div>' + ideas[k].Comments + '</div>' +  '<i class="material-icons">chat_bubble</i>' + '</td>' + 
+				'</div>' +
+
+				'<div class="col s6">' +
 					'<td><div><a href="/user/question">' + ideas[k].Title + '</a></div>' + ideas[k].Description + '</td>' +
 				'</div>' +
-				'<div class="col s3 width3">' + 
+				'<div class="col s3">' + 
 					'<td><div>' + ideas[k].Time + '</div>' + ideas[k].User_Id + '</td>' +
 				'</div>' +
 			'</div>');
