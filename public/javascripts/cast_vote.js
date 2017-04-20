4b5da97c7ea3d88e9ef2f23f17a7d6c9a5e292d2
Duyile Tolulope Patrick <princericky2000@yahoo.com>
2017-04-20 13:37:38 +0100

    implemented upvotes and downvotes

--- public/javascripts/cast_vote.js
+++ public/javascripts/cast_vote.js
@@ -1,1 +1,44 @@
-cast_vote
+$(document).ready(function() {
+
+	const rootRef = firebase.database().ref();
+	
+
+	$('#upvotes').on('click', () => {
+
+		var title = $('#view-title').text();
+		console.log(title);
+
+		var ideaRef = rootRef.child('ideas').orderByChild('Title')
+							.equalTo(title)
+							.limitToFirst(1)
+							.once('value', (snap) => {
+								data = snap.val();
+								//console.log(data);
+
+								var keys = Object.keys(data);
+								var key = keys[0];
+
+								var upvoteRef = rootRef.child('ideas').child(key).update({ Upvotes: data[key]['Upvotes'] + 1 });
+							});
+	});
+
+	$('#downvotes').on('click', () => {
+		var title = $('#view-title').text();
+
+		var title = $('#view-title').text();
+		console.log(title);
+
+		var ideaRef = rootRef.child('ideas').orderByChild('Title')
+							.equalTo(title)
+							.limitToFirst(1)
+							.once('value', (snap) => {
+								data = snap.val();
+								//console.log(data);
+
+								var keys = Object.keys(data);
+								var key = keys[0];
+
+								var downvoteRef = rootRef.child('ideas').child(key).update({ Downvotes: data[key]['Downvotes'] + 1 });
+							});
+	});	
+});
